c25a1a53be646da05906b94117f73654ef315982
Ygot <yrmuller@yahoo.com.br>
2019-10-17 22:00:11 -0300

    live-ux

--- ManipulandoDom/index.js
+++ ManipulandoDom/index.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 
 const server = express(); //chamando a função express
-
-server.get('/teste',(req,res) => {
-    return res.json({ message: 'Hello World' });
+//Query params = ?teste=1
+server.get('/projects',(req,res) => {
+    const nome = req.query.nome;
+    return res.json({ message: 'Projetos e tarefas' });
 })
+//Route params = /users/1
+//Request body = {"nome": "Diego", ...}
 
 server.listen(3000);
